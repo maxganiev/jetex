3808f536cfcc48e1df0cbe2521b1a631a27ffc7f
Max Ganiev <maxganiev@Maxs-MacBook-Air.local>
2024-08-18 16:59:02 +0300

    Seeding db

--- prisma/seeders/seed.js
+++ prisma/seeders/seed.js
@@ -1,11 +1,56 @@
 import { PrismaClient } from '@prisma/client';
+import excelToJson from './excelToJson.js';
 const prisma = new PrismaClient();
 
-async function seed() {
-	const names = ['Foo', 'Bar', 'Hello', 'World', 'John', 'Doe'];
+class Seeder {
+	constructor(modelName = '') {
+		this.path = '/upload/db/' + modelName + '.xlsx';
+		this.modelName = modelName;
 
-	await prisma.test.createMany({ data: names.map((name) => Object.assign({}, { name })) });
-	console.log(await prisma.test.findMany());
+		const res = excelToJson({ filePath: this.path }),
+			keys = Object.keys(res[0]);
+
+		this.data = res.map((item) =>
+			Object.assign(
+				{},
+				{ ...keys.reduce((acc, key) => Object.assign(acc, { [key]: item[key] || null }), {}) }
+			)
+		);
+
+		//console.log(this.data)
+	}
+}
+
+async function seed() {
+	const pump_types = new Seeder('pump_types'),
+		pump_models = new Seeder('pump_models'),
+		pump_duty_points = new Seeder('pump_duty_points'),
+		attribute_groups = new Seeder('attribute_groups'),
+		attribute_values_to_pump_models = new Seeder('attribute_values_to_pump_models'),
+		attributes_to_pump_types = new Seeder('attributes_to_pump_types'),
+		attributes = new Seeder('attributes'),
+		images_to_pump_types = new Seeder('images_to_pump_types'),
+		images_to_pump_models = new Seeder('images_to_pump_models');
+
+	const listOfSeeders = [
+		pump_types,
+		pump_models,
+		pump_duty_points,
+		attribute_groups,
+		attributes,
+		attributes_to_pump_types,
+		images_to_pump_models,
+		images_to_pump_types
+	];
+
+	await prisma.$transaction([
+		...listOfSeeders.map((s) => prisma[s.modelName].createMany({ data: s.data })),
+		prisma.attribute_values_to_pump_models.createMany({
+			data: attribute_values_to_pump_models.data.map((item) =>
+				Object.assign({}, { ...item, value: item.value?.toString() || null })
+			)
+		})
+	]);
 }
 
 seed()
---
+++ src/lib/globals.js
@@ -0,0 +1,3 @@
+export const globals = {
+	imagePath: '/assets/images/',
+};
